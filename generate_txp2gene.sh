043c4b489ad62ff9a243c7cb1dc08217750bba51
lconde-ucl <l.conde@ucl.ac.uk>
2019-10-11 21:18:55 +0100

    fix creation of txt2gene file

--- generate_txp2gene.sh
+++
@@ -1,2 +0,0 @@
-bioawk -c gff '$feature=="transcript" {print $group}' < \(gunzip -c gencode.v32.primary_assembly.annotation.gtf.gz\) | awk -F ' ' '{print substr\($4,2,length\($4\)-3\) "\t" substr\($2,2,length\($2\)-3\)}' - > txp2gene.tsv
-
